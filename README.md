b389ab95ae4bb5d919a49a5e931cd415403fa3ce
Alec Smecher <alec@smecher.bc.ca>
2022-08-05 13:13:41 -0700

    Create acron plugin for submodule use

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # acron
 Acron (cron-less scheduled task execution) plugin for OJS, OMP, and OPS
+
+This plugin is included in OJS, OMP, and OPS; there is no need to install or update it separately.
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file index.php
+ *
+ * Copyright (c) 2013-2022 Simon Fraser University
+ * Copyright (c) 2000-2022 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @brief Wrapper for acron plugin
+ */
+
+return new \APP\plugins\generic\acron\AcronPlugin();
